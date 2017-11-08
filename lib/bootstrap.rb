2fcea295cbb0c622d0faffe33a36640cedea56ee
Paweł J. Wal <pawel.wal@codesthq.com>
2017-11-08 09:08:45 +0100

    Accept v3 media type

--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
+Octokit.default_media_type = 'application/vnd.github.v3+json'
+
 require_relative './pleasebot'
 
 require_relative './redis_service'
